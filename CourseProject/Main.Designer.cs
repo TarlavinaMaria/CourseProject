ae81f2438971f6401ab2b5b04f29a534109cf858
Мария Тарлавина <tarlavina50@mail.ru>
2024-03-28 18:46:25 +0700

    Настройка

--- CourseProject/Main.Designer.cs
+++ CourseProject/Main.Designer.cs
@@ -44,24 +44,24 @@
 			// buttonIncome
 			// 
 			this.buttonIncome.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonIncome.Location = new System.Drawing.Point(17, 73);
+			this.buttonIncome.Location = new System.Drawing.Point(23, 71);
 			this.buttonIncome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.buttonIncome.Name = "buttonIncome";
-			this.buttonIncome.Size = new System.Drawing.Size(168, 45);
+			this.buttonIncome.Size = new System.Drawing.Size(168, 59);
 			this.buttonIncome.TabIndex = 0;
-			this.buttonIncome.Text = "Доход";
+			this.buttonIncome.Text = "Добавить доход";
 			this.buttonIncome.UseVisualStyleBackColor = true;
 			this.buttonIncome.Click += new System.EventHandler(this.buttonIncome_Click);
 			// 
 			// buttonExpense
 			// 
 			this.buttonExpense.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonExpense.Location = new System.Drawing.Point(603, 71);
+			this.buttonExpense.Location = new System.Drawing.Point(655, 73);
 			this.buttonExpense.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.buttonExpense.Name = "buttonExpense";
-			this.buttonExpense.Size = new System.Drawing.Size(168, 45);
+			this.buttonExpense.Size = new System.Drawing.Size(172, 59);
 			this.buttonExpense.TabIndex = 1;
-			this.buttonExpense.Text = "Расход";
+			this.buttonExpense.Text = "Добавить расход";
 			this.buttonExpense.UseVisualStyleBackColor = true;
 			this.buttonExpense.Click += new System.EventHandler(this.buttonExpense_Click);
 			// 
@@ -72,33 +72,32 @@
 			this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dgvInfo.Location = new System.Drawing.Point(17, 204);
 			this.dgvInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.dgvInfo.MultiSelect = false;
 			this.dgvInfo.Name = "dgvInfo";
 			this.dgvInfo.ReadOnly = true;
 			this.dgvInfo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.dgvInfo.Size = new System.Drawing.Size(754, 336);
+			this.dgvInfo.Size = new System.Drawing.Size(810, 371);
 			this.dgvInfo.TabIndex = 3;
 			this.dgvInfo.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInfo_CellDoubleClick);
 			// 
 			// buttonDelete
 			// 
 			this.buttonDelete.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonDelete.Location = new System.Drawing.Point(410, 71);
+			this.buttonDelete.Location = new System.Drawing.Point(454, 71);
 			this.buttonDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.buttonDelete.Name = "buttonDelete";
-			this.buttonDelete.Size = new System.Drawing.Size(168, 45);
+			this.buttonDelete.Size = new System.Drawing.Size(193, 59);
 			this.buttonDelete.TabIndex = 5;
-			this.buttonDelete.Text = "Удалить";
+			this.buttonDelete.Text = "Удалить запись";
 			this.buttonDelete.UseVisualStyleBackColor = true;
 			this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
 			// 
 			// rtbSearch
 			// 
 			this.rtbSearch.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.rtbSearch.Location = new System.Drawing.Point(208, 135);
+			this.rtbSearch.Location = new System.Drawing.Point(212, 140);
 			this.rtbSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.rtbSearch.Name = "rtbSearch";
-			this.rtbSearch.Size = new System.Drawing.Size(563, 46);
+			this.rtbSearch.Size = new System.Drawing.Size(615, 46);
 			this.rtbSearch.TabIndex = 6;
 			this.rtbSearch.Text = "";
 			this.rtbSearch.TextChanged += new System.EventHandler(this.rtbSearch_TextChanged);
@@ -106,22 +105,22 @@
 			// rtbBalance
 			// 
 			this.rtbBalance.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.rtbBalance.Location = new System.Drawing.Point(144, 21);
+			this.rtbBalance.Location = new System.Drawing.Point(212, 18);
 			this.rtbBalance.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.rtbBalance.Name = "rtbBalance";
 			this.rtbBalance.ReadOnly = true;
 			this.rtbBalance.RightToLeft = System.Windows.Forms.RightToLeft.No;
-			this.rtbBalance.Size = new System.Drawing.Size(627, 42);
+			this.rtbBalance.Size = new System.Drawing.Size(615, 42);
 			this.rtbBalance.TabIndex = 2;
 			this.rtbBalance.Text = "";
 			// 
 			// btnAddCategory
 			// 
 			this.btnAddCategory.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.btnAddCategory.Location = new System.Drawing.Point(199, 71);
+			this.btnAddCategory.Location = new System.Drawing.Point(212, 71);
 			this.btnAddCategory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.btnAddCategory.Name = "btnAddCategory";
-			this.btnAddCategory.Size = new System.Drawing.Size(191, 45);
+			this.btnAddCategory.Size = new System.Drawing.Size(216, 59);
 			this.btnAddCategory.TabIndex = 7;
 			this.btnAddCategory.Text = "Добавить категорию";
 			this.btnAddCategory.UseVisualStyleBackColor = true;
@@ -131,7 +130,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(13, 148);
+			this.label1.Location = new System.Drawing.Point(17, 153);
 			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(187, 21);
@@ -141,11 +140,11 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label2.Location = new System.Drawing.Point(17, 21);
+			this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(28, 18);
 			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(69, 21);
+			this.label2.Size = new System.Drawing.Size(99, 31);
 			this.label2.TabIndex = 9;
 			this.label2.Text = "Баланс:";
 			// 
@@ -153,7 +152,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.ClientSize = new System.Drawing.Size(834, 611);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.btnAddCategory);
@@ -166,8 +165,8 @@
 			this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
-			this.MaximumSize = new System.Drawing.Size(800, 600);
-			this.MinimumSize = new System.Drawing.Size(800, 600);
+			this.MaximumSize = new System.Drawing.Size(850, 650);
+			this.MinimumSize = new System.Drawing.Size(850, 650);
 			this.Name = "Main";
 			this.Text = "Учет доходов и расходов";
 			((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
